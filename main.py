fdf3084207529dd99e4d00558c95ad607177db18
Max Valek <valek.maxwell@gmail.com>
2023-07-27 15:43:49 -0500

    Implement feature to get top songs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from networking.api_client import ApiClient
 from networking.endpoints import Endpoints
 from dataManager.artists_manager import ArtistsManager
+from dataManager.songs_manager import SongsManager
 
 import json
 
@@ -8,14 +9,15 @@ if __name__ == "__main__":
 
     api_client = ApiClient()
 
-    response = api_client.get(Endpoints.Artists.TOP)
+    # response = api_client.get(Endpoints.Artists.TOP)
+    response = api_client.get(Endpoints.Songs.TOP)
 
-    with open("response.json", "w") as outfile:
-        json.dump(response, outfile)
+    # with open("response.json", "w") as outfile:
+    #     json.dump(response, outfile)
 
-    ArtistsManager().print_top_artists(response)
+    # ArtistsManager().print_top_artists(response)
 
-    # print(data)
+    SongsManager().print_top_songs(response)
 
     
     
