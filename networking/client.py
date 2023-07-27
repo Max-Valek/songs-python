b1c2ad0e16aaf77d8c8e284ca0695c4c700af5d5
Max Valek <valek.maxwell@gmail.com>
2023-07-27 17:38:20 -0500

    documentation

--- networking/client.py
+++ networking/client.py
@@ -9,17 +9,20 @@ load_dotenv()   # load environment variables from .env file
 class ApiClient:
 
     def __init__(self) -> None:
-        # Environment variables
+        # Load environment variables
         self.api_key = os.getenv("API_KEY")
         self.base_url = os.getenv("BASE_URL")
+
+        # Unable to load from env
         if not self.api_key or not self.base_url:
             raise ValueError("Could not get environment variables.")
     
     def get(self, endpoint):
         url = f"{self.base_url}{endpoint}&apikey={self.api_key}"
+        # Make request and print a message for the status code
         response = requests.get(url)
         print(Status.get_message(response.status_code))
-
+        # Return the response as json or raise an error
         if response.ok:
             return response.json()
         else:
